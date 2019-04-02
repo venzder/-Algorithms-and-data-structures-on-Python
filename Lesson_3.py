42bf3d4af22ba8c08df8e4e596ca9b48af2103ee
Sergey <sergivanov@list.ru>
2019-04-02 23:03:18 +0300

    Решил задачу №4

--- Lesson_3.py
+++ Lesson_3.py
@@ -27,18 +27,41 @@
 
 # 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 
-import random
-
-x = [random.randint(1, 99) for i in range(10)]
-print(x)
-print(max(x))
-print(min(x))
-a = x.index(min(x))
-b = x.index(max(x))
-x[a], x[b] = x[b], x[a]
-print(x)
+# import random
+#
+# x = [random.randint(1, 99) for i in range(10)]
+# print(x)
+# print(max(x))
+# print(min(x))
+# a = x.index(min(x))
+# b = x.index(max(x))
+# x[a], x[b] = x[b], x[a]
+# print(x)
 
 # 4. Определить, какое число в массиве встречается чаще всего.
+
+import random
+
+x = [random.randint(1, 10) for i in range(100)]
+counts = []
+for i in x:
+    count = 0
+    for j in x:
+        if i == j:
+            count += 1
+    counts.append((i, count))
+counts = set(counts)
+count_max = []
+for i in counts:
+    count_max.append(i[1])
+maxim = max(count_max)
+if maxim <= 1:
+    print('Все значения уникальны')
+else:
+    for i in counts:
+        if i[1] == maxim:
+            print(f'Число {i[0]} встречается максимальные {i[1]} раз')
+
 # 5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию (индекс) в массиве.
 # 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами. Сами
 # минимальный и максимальный элементы в сумму не включать.
